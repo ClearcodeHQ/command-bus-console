82cbbefa9da71ead5fb4105648868726fdc7d10e
lucaszz <lucaszz111@gmail.com>
2015-11-02 23:47:31 +0100

    DoctrineORM removed from tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,5 @@
 if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
-$loader = include $autoloadFile;
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+require $autoloadFile;
